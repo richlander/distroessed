ead1125534c1123325589ab0075cf05cb705eb24
Richard Lander <rlander@microsoft.com>
2024-07-30 07:18:34 -0700

    Add obsolete attributes

--- src/DotnetRelease/MajorReleasesIndexRecords.cs
+++ src/DotnetRelease/MajorReleasesIndexRecords.cs
@@ -44,7 +44,8 @@ public record MajorReleaseIndexItem(
     ReleaseType ReleaseType,
 
     [property: Description("Link to detailed release descriptions (JSON format), with all patch releases in one file. This property is now deprecated, but still required (for compatibility)."),
-        JsonPropertyName("releases.json")]
+        JsonPropertyName("releases.json"),
+        Obsolete("This property is obsolete. Use PatchReleasesInfoUri instead.")]
     string ReleasesJson,
 
     [property: Description("Link to detailed release descriptions (JSON format), with all patch releases in one file.")]
@@ -56,7 +57,8 @@ public record MajorReleaseIndexItem(
 
     [property: Description("Link to supported OS matrix (JSON format). This property is now deprecated, but still required (for compatibility)."),
         JsonPropertyName("supported-os.json"),
-        JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull),
+        Obsolete("This property is obsolete. Use SupportedOsInfoUri instead.")]
     string? SupportedOsJson = null,
 
     [property: Description("Link to supported OS matrix (JSON format)."),
