135adbc3cd5592face79aab02c887bbb0ca3f103
Rich Lander <rich@lander.ca>
2025-07-05 19:54:23 -0700

    Update IndexHelpers

--- src/UpdateIndexes/IndexHelpers.cs
+++ src/UpdateIndexes/IndexHelpers.cs
@@ -127,13 +127,6 @@ public class IndexHelpers
 
     public static string GetProdPath(string relativePath) => GetRawGitHubBranchPath(relativePath);
 
-    public static string GetCdnPath(string relativePath) =>
-      $"https://builds.dotnet.microsoft.com/dotnet/release-metadata/{relativePath}";
-
-
-    public static string GetRawGitHubPath(string relativePath) =>
-      $"https://raw.githubusercontent.com/dotnet/core/main/release-notes/{relativePath}";
-
     public static string GetRawGitHubBranchPath(string relativePath) =>
       $"https://raw.githubusercontent.com/richlander/core/main/release-notes/{relativePath}";
 
